d667cdab19eeaed41fc6996d175dd45d9feddd72
karlaevelize <k.evelize@gmail.com>
2022-03-16 15:25:24 +0100

    added middlewares

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ const express = require("express")
 const userRouter = require("./routers/users")
 
 //Start an express server
-//Write 4 endpoints: Createn(post), Read(get), Update(put), Delete(delete)
+//Write 4 endpoints: Create(post), Read(get), Update(put), Delete(delete)
 
 const app = express()
 const PORT = 4000
 
+//middlewares
 app.use(express.json()) //parse the body
 
+const myMiddleware = (request, response, next) => {
+  console.log("middleware was triggered")
+  next()
+}
+
+const randomMiddleware = (request, response, next) => {
+  const randomNumber = Math.random() * 10
+  console.log(randomNumber)
+
+  if (randomNumber <= 5){
+    console.log("welcome to my middleware")
+    next()
+  } else {
+    response.status(401).end()
+  }
+
+}
+
 //welcome route
-app.get("/", (request, response, next) => {
+app.get("/", randomMiddleware, (request, response, next) => {
   try {
     response.send("Welcome to my API")
   } catch (e) {
@@ -19,7 +38,7 @@ app.get("/", (request, response, next) => {
   }
 })
 
-app.use("/users", userRouter)
+app.use("/users", myMiddleware, userRouter)
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
